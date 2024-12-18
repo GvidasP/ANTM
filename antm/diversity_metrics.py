7fa20cfa90b1aaa67b365ed55ce73230d8df98cd
GvidasP <gvidaspran@gmail.com>
2024-12-18 11:51:03 +0200

    topic repr

--- antm/diversity_metrics.py
+++ antm/diversity_metrics.py
@@ -1,7 +1,6 @@
 from itertools import combinations
 
 
-
 def proportion_unique_words(topics, topk=10):
     """
     compute the proportion of unique words
@@ -12,7 +11,7 @@ def proportion_unique_words(topics, topk=10):
     topk: top k words on which the topic diversity will be computed
     """
     if topk > len(topics[0]):
-        raise Exception('Words in topics are less than '+str(topk))
+        raise Exception("Words in topics are less than " + str(topk))
     else:
         unique_words = set()
         for topic in topics:
@@ -22,24 +21,25 @@ def proportion_unique_words(topics, topk=10):
 
 
 def pairwise_jaccard_diversity(topics, topk=10):
-    '''
-    compute the average pairwise jaccard distance between the topics 
-  
+    """
+    compute the average pairwise jaccard distance between the topics
+
     Parameters
     ----------
     topics: a list of lists of words
     topk: top k words on which the topic diversity
           will be computed
-    
+
     Returns
     -------
     pjd: average pairwise jaccard distance
-    '''
+    """
     dist = 0
     count = 0
     for list1, list2 in combinations(topics, 2):
-        js = 1 - len(set(list1).intersection(set(list2)))/len(set(list1).union(set(list2)))
+        js = 1 - len(set(list1).intersection(set(list2))) / len(
+            set(list1).union(set(list2))
+        )
         dist = dist + js
         count = count + 1
-    return dist/count
-
+    return dist / count
--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='antm',
-    version='0.1.1',
-    author='Hamed Rahimi',
-    author_email='<hamed.rahimi@sorbonne-universite.fr',
-    description='Aligned Neural Topic Model for Exploring Evolving Topics',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/hamedR96/ANTM',
+    name="antm",
+    version="0.1.1",
+    author="Hamed Rahimi",
+    author_email="<hamed.rahimi@sorbonne-universite.fr",
+    description="Aligned Neural Topic Model for Exploring Evolving Topics",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hamedR96/ANTM",
     packages=find_packages(),
-    install_requires=["hdbscan","swifter","sentence-transformers","umap-learn","transformers","torch",
-                      "gensim","plotly","matplotlib","pyarrow","kaleido"
-                      ],
-    
-    project_urls={
-        'Bug Tracker': 'https://github.com/hamedR96/ANTM/issues'
-    },
+    install_requires=[
+        "hdbscan",
+        "swifter",
+        "sentence-transformers",
+        "umap-learn",
+        "transformers",
+        "torch",
+        "gensim",
+        "plotly",
+        "matplotlib",
+        "pyarrow",
+        "kaleido",
+    ],
+    project_urls={"Bug Tracker": "https://github.com/hamedR96/ANTM/issues"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -24,5 +31,4 @@ setup(
         "Operating System :: Unix ",
         "Operating System :: Microsoft :: Windows ",
     ],
-
 )
